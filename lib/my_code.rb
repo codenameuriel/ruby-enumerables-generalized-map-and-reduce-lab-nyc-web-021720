10caef1d80bbd7261eaa8265f0a9076b6dc2e3e6
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-02 19:34:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
 # Your Code Here
+
+def map(source_array)
+  squared_array = []
+  array_index = 0 
+  while array_index < source_array.length do 
+    squared_array << yield(source_array[array_index])
+    array_index += 1 
+  end
+  return squared_array
+end
+
+# def reduce(source_array, starting_point = 0)
+#   total = starting_point
+#   i = 0
+#   while i < source_array.length do
+#     total = yield(total, source_array[i])
+#     i += 1 
+#   end
+#   return total
+# end
+
+def reduce(array, sv = nil)
+    if sv
+        sum = sv 
+        i = 0
+    else
+        sum = array[0]
+        i = 1
+    end
+
+    while i < array.length do
+        sum = yield(sum, array[i])
+        i += 1
+    end
+    return sum
+end
